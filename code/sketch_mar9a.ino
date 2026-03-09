2d64e247998f8d27880b1ff80afef85022a2967b
FloweyGaming <86134968+FloweyGaming@users.noreply.github.com>
2026-03-09 15:27:02 +0100

    Add files via upload

--- code/sketch_mar9a.ino
+++ code/sketch_mar9a.ino
@@ -1,15 +1,28 @@
-
 #include <Ultrasonic.h>
 
-Ultrasonic ultrasonic1(12, 13);	// An ultrasonic sensor HC-04
-
+Ultrasonic ultrasonic1(12, 13);  // Capteur HC-SR04
+#define Led 5
 
 void setup() {
   Serial.begin(9600);
+  pinMode(Led, OUTPUT);
 }
 
 void loop() {
+
+  int distance = ultrasonic1.distanceRead();
+
   Serial.print("Sensor: ");
-  Serial.print(ultrasonic1.distanceRead()); // Prints the distance on the default unit (centimeters)
-  Serial.println("cm");
+  Serial.print(distance);
+  Serial.println(" cm");
+  // Si un objet est à moins de 20 cm, on allume la LED
+  if (distance < 20 || distance > 300) {
+    digitalWrite(Led, HIGH);
+  } 
+  else {
+    digitalWrite(Led, LOW);
+  }
+
+
+  delay(500);
 }
