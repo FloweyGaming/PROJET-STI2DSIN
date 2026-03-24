34d050d1f58e46e99edc76e9105cb410f0d25d2a
FloweyGaming <86134968+FloweyGaming@users.noreply.github.com>
2026-03-24 08:53:19 +0100

    Refactor sensor readings and improve logic flow

--- code/sketch_mar9a.ino
+++ code/sketch_mar9a.ino
@@ -1,56 +1,88 @@
+/*****************
+----LIBRAIRIES----
+*****************/
 #include <Ultrasonic.h>
+#include <arduino-timer.h>
 
-int press;
+/***********************
+----SETUP COMPOSANTS----
+***********************/
+int pression;
 float VCC = 5.0;
 float VOUT;
 float RC;
+auto timer = timer_create_default();
 
-Ultrasonic ultrasonic1(12, 13); //CapUS = PIN 12 & 13
-#define Led 5 //LED = PIN 5
+/*************************
+----DÉFINITIONS DE PIN----
+*************************/
+Ultrasonic ultrasonic1(12, 13); //CapUS = PIN 12 (TRIG) & 13 (ECHO)
+#define led 5 //LED = PIN 5
 #define buzzer 6 //BUZZER = PIN 6
+int press = A2; //CAPTEUR PRESSION = PIN A2
+int motor = A0; //MOTEUR = PIN A0
 
+/**********************
+----BOUCLE INITIALE----
+**********************/
 void setup() {
-  pinMode(A0, INPUT); //CAPTEUR PRESSION = ENTRÉE
+  pinMode(motor, OUTPUT); //MOTEUR = SORTIE
+  pinMode(press, INPUT); //CAPTEUR PRESSION = ENTRÉE
   pinMode(led, OUTPUT); //LED = SORTIE
   pinMode(buzzer, OUTPUT); //BUZZER = SORTIE
   Serial.begin(9600);
 }
 
-void loop() {
-
+/******************************
+----SI DISTANCE TROP COURTE----
+******************************/
+int tooclose(){
   int distance = ultrasonic1.distanceRead(); // Var "Distance" = Distance Renvoyée par CapUS
-  press = analogRead(A0); // Var "Pression" = Force Renvoyée par CapPR
+  if (distance < 20 || distance > 300) {
+    analogWrite(A0, 255);
+    digitalWrite(buzzer, HIGH); // Si un objet est à moins de 20 cm ET 2 secondes sont passées, on allume le Buzzer
+    delay(500);
+    digitalWrite(buzzer, LOW);
+    delay(500);
+    }
+}
 
+/************************
+----BOUCLE PRINCIPALE----
+************************/
+void loop() {
+	int distance = ultrasonic1.distanceRead(); // Var "Distance" = Distance Renvoyée par CapUS
+  pression = analogRead(A2); // Var "Pression" = Force Renvoyée par CapPR
+
+  /***************************
+  ----ÉCRITURE DES VALEURS----
+  ***************************/
   Serial.print("Distance: ");
   Serial.print(distance);
   Serial.println(" cm"); //Renvoie "<distance> cm"
 
   Serial.print("Poids: ");
-  Serial.print(map(press, 0, 1023, 0, 20000)); //Convertir les Valeurs de tensions en grammes
+  Serial.print(map(pression, 0, 1023, 0, 20000)); //Convertir les Valeurs de tensions en grammes (max = 2kg)
   Serial.println(" g"); //Renvoie "<pression> g"
 
-  // Détection distance
+  /****************************
+  ----DÉTÉCTION DE DISTANCE----
+  ****************************/
   if (distance < 20 || distance > 300) {
-    delay(2500);
-    Serial.print(distance);
-    Serial.println(" cm");
-    if (distance < 20 || distance > 300) {
-      digitalWrite(buzzer, HIGH); // Si un objet est à moins de 20 cm ET 2 secondes sont passées, on allume la LED
-      delay(500);
-      digitalWrite(buzzer, LOW);
-      delay(500); 
-    }
-      else {digitalWrite(buzzer, LOW); //Sinon, éteindre (ou laisser éteint) le Buzzer
-      }
-  } else {
+    timer.in(2500, tooclose());
+  } 
+  else {
     digitalWrite(buzzer, LOW); //Sinon, éteindre (ou laisser éteint) le Buzzer
+    analogWrite(A0, 150);
   }
 
-  // Détection pression
-  if (press > 511) {
-    digitalWrite(Led, HIGH); // Si pression appliquée est supérieure à 511g, on allume la LED
+  /*****************************
+  ----DÉTÉCTION DE PRESSSION----
+  *****************************/
+  if (pression > 511) {
+    digitalWrite(led, HIGH); // Si pression appliquée est supérieure à 511g, on allume la LED
   } else {
-    digitalWrite(Led, LOW); //Sinon, éteindre (ou laisser éteinte) la LED
+    digitalWrite(led, LOW); //Sinon, éteindre (ou laisser éteinte) la LED
   }
 
   delay(200);
